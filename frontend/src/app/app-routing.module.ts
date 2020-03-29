7b9b3665d4cb498ce676961907e06fac778230dc
hidayat <misterpy@gmail.com>
2020-03-29 04:26:16 +0200

    lazy load thermostat module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../features/thermostat/thermostat.module').then(m => m.ThermostatModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
