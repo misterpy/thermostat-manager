c06282c6c5c021dbc22649e7bb084cdb774b9a9e
hidayat <misterpy@gmail.com>
2020-03-29 08:51:35 +0200

    adds the endpoint to create thermostat

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 
 const thermostats = [
     {
@@ -180,8 +181,7 @@ app.get('/thermostats/:id/measurements', (req, res) => {
 });
 
 app.post('/thermostats', (req, res) => {
-    console.log('req body >>>', req.body);
-    res.status(200).send(createThermostat(res.body));
+    res.status(200).send(createThermostat(req.body));
 });
 
 app.listen(port, () => console.log(`Thermostat backend is listening on port ${port}!`));
